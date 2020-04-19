f908b4496db846344219eb61b87068f38df186e4
kausar2697 <17103126@iubat.edu>
2020-04-19 19:49:56 +0600

    Updates

---
+++ precache-manifest.d1d1085bccb538d4aaaf7a5a79158757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a38f9eab7e00db83b74c7a987de1db1",
+    "url": "/book-ocean/index.html"
+  },
+  {
+    "revision": "42e211a68886872d28f4",
+    "url": "/book-ocean/static/css/2.a8ba0f7c.chunk.css"
+  },
+  {
+    "revision": "0dfd50ecfb33da45a587",
+    "url": "/book-ocean/static/css/main.86a4acdf.chunk.css"
+  },
+  {
+    "revision": "42e211a68886872d28f4",
+    "url": "/book-ocean/static/js/2.857712a8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/book-ocean/static/js/2.857712a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfd50ecfb33da45a587",
+    "url": "/book-ocean/static/js/main.ada4faeb.chunk.js"
+  },
+  {
+    "revision": "2d6883085c815eb8fa8b",
+    "url": "/book-ocean/static/js/runtime-main.c05cd4d0.js"
+  }
+]);
